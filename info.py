72d526dad9d171a85d350631692098e4757d025f
MeDott29 <mhendricks1290@gmail.com>
2025-03-21 02:23:53 -0400

    clean

---
+++ info.py
@@ -0,0 +1,12 @@
+from datasets import load_dataset
+
+dataset = load_dataset("the-ride-never-ends/american_law", split="train", streaming=True)
+
+# Get the first batch to inspect column names
+first_batch = next(iter(dataset))
+print(first_batch.keys())
+
+# Print the first 5 lines
+dataset_iter = iter(dataset)
+for _ in range(5):
+    print(next(dataset_iter))
